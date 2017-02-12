400a3f903496cc0a8540ded759e7562f8a37d98e
Asia <gibsonjasia@gmail.com>
2017-02-12 16:40:03 -0500

    xml file added

--- app/src/main/java/com/example/asiagibson/profile/MainScreenActivity.java
+++ app/src/main/java/com/example/asiagibson/profile/MainScreenActivity.java
@@ -1,6 +1,7 @@
 package com.example.asiagibson.profile;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
@@ -8,10 +9,19 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.zip.Inflater;
+
+import static com.example.asiagibson.profile.R.array.list_items;
 
 /**
  * Created by asiagibson on 2/12/17.
@@ -19,9 +29,13 @@ import android.widget.TextView;
 
 public class MainScreenActivity extends ActionBarActivity {
 
-     DrawerLayout drawerLayout;
-     ListView listView;
+    NavViewAdapter adapter;
+    DrawerLayout drawerLayout;
+    ListView listView;
 
+    String iconText [] = {"Button 1", "Button 2", "Button 3","Button 4"};
+    int iconList[] = {R.drawable.heart, R.drawable.email,
+            R.drawable.addressbook, R.drawable.logout};
 
     Button heartB, emailB, addressbkB, logoutB;
     TextView hTV, eTV, abkTV, logoutTv;
@@ -33,6 +47,10 @@ public class MainScreenActivity extends ActionBarActivity {
 
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         listView = (ListView) findViewById(R.id.drawerlist);
+        adapter = new NavViewAdapter(getApplicationContext(), iconList,iconText);
+        listView.setAdapter(adapter);
+
     }
 }
+
 
