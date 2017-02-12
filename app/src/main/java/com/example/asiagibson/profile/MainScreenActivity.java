89e6d27f3c665b752a21dfd658d086367e6cff39
Asia <gibsonjasia@gmail.com>
2017-02-12 13:17:44 -0500

    safe commit

--- app/src/main/java/com/example/asiagibson/profile/MainScreenActivity.java
+++ app/src/main/java/com/example/asiagibson/profile/MainScreenActivity.java
@@ -2,16 +2,30 @@ package com.example.asiagibson.profile;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
 /**
  * Created by asiagibson on 2/12/17.
  */
 
-public class MainScreenActivity extends Activity {
+public class MainScreenActivity extends AppCompatActivity {
+
+    Button heartB, emailB, addressbkB, logoutB;
+    TextView hTV, eTV, abkTV, logoutTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+
     }
 }
+
